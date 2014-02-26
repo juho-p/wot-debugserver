3a8a6d79d8a5310caf8efada0fd46a0848ef3dcb
Juho Peltonen <juhopel@gmail.com>
2014-02-26 16:44:26 +0200

    Useful utility function

--- wotdbg.py
+++ wotdbg.py
@@ -1,11 +1,20 @@
-import tcprepl
+import os.path
 
+import tcprepl
 import BigWorld
 
 def echo(s):
     if tcprepl.write_client is not None:
         tcprepl.write_client(s)
 
-def player():
-    return BigWorld.player()
+def exec_file(filename, exec_globals=None):
+    if exec_globals is None:
+        exec_globals = {}
 
+    if not os.path.isfile(filename):
+        filename = BigWorld.wg_resolveFileName(filename)
+
+    with open(filename, 'r') as f:
+        code = f.read()
+
+    exec code in exec_globals
