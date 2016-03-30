2a630e283fd57d45c909b882aacec3a5c298b028
Juho Peltonen <juhopel@gmail.com>
2016-03-30 15:27:07 +0300

    add folder creation to loader install script

--- loader/install_loader.sh
+++ loader/install_loader.sh
@@ -9,4 +9,6 @@ resmods='../../../..'
 # pretty crap, but whatever
 lastversion=`(cd $resmods && ls | grep '^[0-9]\.[0-9]' | sort -V | tail -n1)`
 
-cp -v mod_.pyc $resmods/$lastversion/scripts/client/gui/mods/
+installdir="$resmods/$lastversion/scripts/client/gui/mods/"
+mkdir -pv "$installdir"
+cp -v mod_.pyc "$installdir"
